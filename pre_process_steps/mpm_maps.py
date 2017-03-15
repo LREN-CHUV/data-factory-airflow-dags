d8614b9908443cfea4ff70fb046c387d2a7472ca
Mirco Nasuti <mirco.nasuti@chuv.ch>
2017-03-15 16:23:07 +0100

    extract neuro_morphometric_atlas

--- pre_process_steps/mpm_maps.py
+++ pre_process_steps/mpm_maps.py
@@ -8,6 +8,7 @@
   * PIPELINES_PATH
   * MPM_MAPS_SPM_FUNCTION
   * MPM_MAPS_LOCAL_FOLDER
+  * MPM_MAPS_SERVER_FOLDER
 
 """
 
@@ -24,31 +25,33 @@ from common_steps import Step, default_config
 
 def mpm_maps_pipeline_cfg(dag, upstream_step, dataset_section):
     default_config(dataset_section, 'DATASET_CONFIG', '')
-    default_config(dataset_section, 'MPM_MAPS_SPM_FUNCTION', 'selectT1')
 
     dataset_config = configuration.get(dataset_section, 'DATASET_CONFIG')
     pipeline_path = configuration.get(dataset_section, 'PIPELINES_PATH') + '/MPMs_Pipeline'
     misc_library_path = configuration.get(dataset_section, 'PIPELINES_PATH') + '/../Miscellaneous&Others'
     spm_function = configuration.get(dataset_section, 'MPM_MAPS_SPM_FUNCTION')
     local_folder = configuration.get(dataset_section, 'MPM_MAPS_LOCAL_FOLDER')
+    server_folder = configuration.get(dataset_section, 'MPM_MAPS_SERVER_FOLDER')
 
     return mpm_maps_pipeline(dag, upstream_step,
                              dataset_config=dataset_config,
                              pipeline_path=pipeline_path,
                              misc_library_path=misc_library_path,
                              spm_function=spm_function,
-                             local_folder=local_folder)
+                             local_folder=local_folder,
+                             server_folder=server_folder)
 
 
 def mpm_maps_pipeline(dag, upstream_step,
                       dataset_config=None,
-                      spm_function='selectT1',
+                      spm_function='mpm_maps',
                       pipeline_path=None,
                       misc_library_path=None,
                       local_folder=None,
-                      protocols_file=None):
+                      protocols_file=None,
+                      server_folder=None):
 
-    def arguments_fn(folder, session_id, **kwargs):
+    def arguments_fn(folder, session_id, pipeline_params_config_file='Preproc_mpm_maps_pipeline_config.txt', **kwargs):
         """
           Prepare the arguments for the pipeline that selects T1 files from DICOM.
           It selects all T1 files located in the folder 'folder'
@@ -56,9 +59,11 @@ def mpm_maps_pipeline(dag, upstream_step,
         parent_data_folder = os.path.abspath(folder + '/..')
 
         return [parent_data_folder,
-                local_folder,
                 session_id,
-                protocols_file]
+                local_folder,
+                protocols_file,
+                pipeline_params_config_file,
+                server_folder]
 
     mpm_maps_pipeline = SpmPipelineOperator(
         task_id='mpm_maps_pipeline',
@@ -66,10 +71,10 @@ def mpm_maps_pipeline(dag, upstream_step,
         spm_arguments_callable=arguments_fn,
         matlab_paths=[misc_library_path, pipeline_path],
         output_folder_callable=lambda session_id, **kwargs: local_folder + '/' + session_id,
-        pool='image_preprocessing',
         parent_task=upstream_step.task_id,
         priority_weight=upstream_step.priority_weight,
         execution_timeout=timedelta(hours=24),
+        pool='image_preprocessing',
         on_skip_trigger_dag_id='mri_notify_skipped_processing',
         on_failure_trigger_dag_id='mri_notify_failed_processing',
         dataset_config=dataset_config,
@@ -79,19 +84,20 @@ def mpm_maps_pipeline(dag, upstream_step,
     mpm_maps_pipeline.set_upstream(upstream_step.task)
 
     mpm_maps_pipeline.doc_md = dedent("""\
-        # MPM Maps Pipeline
+            # MPM Maps Pipeline
 
-        SPM function: __%s__
+            SPM function: __%s__
 
-        This function computes the Multiparametric Maps (MPMs) (R2*, R1, MT, PD) and brain segmentation in different
-        tissue maps.
-        All computation was programmed based on the LREN database structure.
+            This function computes the Multiparametric Maps (MPMs) (R2*, R1, MT, PD) and brain segmentation in
+            different tissue maps.
+            All computation was programmed based on the LREN database structure.
 
-        The MPMs are calculated locally and finally copied to a remote folder:
+            The MPMs are calculated locally and finally copied to a remote folder:
 
-        * Local folder: __%s__
+            * Local folder: __%s__
+            * Remote folder: __%s__
 
-        Depends on: __%s__
-        """ % (spm_function, local_folder, upstream_step.task_id))
+            Depends on: __%s__
+            """ % (spm_function, local_folder, server_folder, upstream_step.task_id))
 
     return Step(mpm_maps_pipeline, 'mpm_maps_pipeline', upstream_step.priority_weight + 10)
---
+++ pre_process_steps/neuro_morphometric_atlas.py
@@ -0,0 +1,121 @@
+"""
+
+  Pre processing step: Neuromorphometric Atlas
+
+  Configuration variables used:
+
+  * DATASET_CONFIG
+  * PIPELINES_PATH
+  * NEURO_MORPHOMETRIC_ATLAS_SPM_FUNCTION
+  * NEURO_MORPHOMETRIC_ATLAS_LOCAL_FOLDER
+  * NEURO_MORPHOMETRIC_ATLAS_SERVER_FOLDER
+  * NEURO_MORPHOMETRIC_ATLAS_TPM_TEMPLATE
+
+"""
+
+import os
+
+from datetime import timedelta
+from textwrap import dedent
+
+from airflow import configuration
+from airflow_spm.operators import SpmPipelineOperator
+from common_steps import Step, default_config
+
+
+def neuro_morphometric_atlas_pipeline_cfg(dag, upstream_step, dataset_section):
+    default_config(dataset_section, 'DATASET_CONFIG', '')
+
+    dataset_config = configuration.get(dataset_section, 'DATASET_CONFIG')
+    neuro_morphometric_atlas_pipeline_path = configuration.get(
+        dataset_section,
+        'PIPELINES_PATH') + '/NeuroMorphometric_Pipeline/NeuroMorphometric_tbx/label'
+    mpm_maps_pipeline_path = configuration.get(dataset_section, 'PIPELINES_PATH') + '/MPMs_Pipeline'
+    misc_library_path = configuration.get(dataset_section, 'PIPELINES_PATH') + '/../Miscellaneous&Others'
+    protocols_file = configuration.get(dataset_section, 'PROTOCOLS_FILE')
+    spm_function = configuration.get(dataset_section, 'NEURO_MORPHOMETRIC_ATLAS_SPM_FUNCTION')
+    local_folder = configuration.get(dataset_section, 'NEURO_MORPHOMETRIC_ATLAS_LOCAL_FOLDER')
+    server_folder = configuration.get(dataset_section, 'NEURO_MORPHOMETRIC_ATLAS_SERVER_FOLDER')
+    tpm_template = configuration.get(dataset_section, 'NEURO_MORPHOMETRIC_ATLAS_TPM_TEMPLATE')
+
+    return neuro_morphometric_atlas_pipeline(dag, upstream_step,
+                                             dataset_config=dataset_config,
+                                             neuro_morpho_atlas_pipeline_path=neuro_morphometric_atlas_pipeline_path,
+                                             mpm_maps_pipeline_path=mpm_maps_pipeline_path,
+                                             misc_library_path=misc_library_path,
+                                             spm_function=spm_function,
+                                             local_folder=local_folder,
+                                             server_folder=server_folder,
+                                             tpm_template=tpm_template,
+                                             protocols_file=protocols_file)
+
+
+def neuro_morphometric_atlas_pipeline(dag, upstream_step,
+                                      dataset_config=None,
+                                      neuro_morpho_atlas_pipeline_path=None,
+                                      mpm_maps_pipeline_path=None,
+                                      misc_library_path=None,
+                                      spm_function='neuromorphometric_a',
+                                      local_folder=None,
+                                      server_folder='',
+                                      tpm_template='',
+                                      protocols_file=None):
+
+    def arguments_fn(folder, session_id, **kwargs):
+        """
+          Prepare the arguments for the pipeline that selects T1 files from DICOM.
+          It selects all T1 files located in the folder 'folder'
+        """
+        parent_data_folder = os.path.abspath(folder + '/..')
+        table_format = 'csv'
+
+        return [session_id,
+                parent_data_folder,
+                local_folder,
+                server_folder,
+                protocols_file,
+                table_format,
+                tpm_template]
+
+    neuro_morphometric_atlas_pipeline = SpmPipelineOperator(
+        task_id='neuro_morphometric_atlas_pipeline',
+        spm_function=spm_function,
+        spm_arguments_callable=arguments_fn,
+        matlab_paths=[misc_library_path,
+                      neuro_morpho_atlas_pipeline_path,
+                      mpm_maps_pipeline_path],
+        output_folder_callable=lambda session_id, **kwargs: (local_folder + '/' + session_id),
+        pool='image_preprocessing',
+        parent_task=upstream_step.task_id,
+        priority_weight=upstream_step.priority_weight,
+        execution_timeout=timedelta(hours=24),
+        on_skip_trigger_dag_id='mri_notify_skipped_processing',
+        on_failure_trigger_dag_id='mri_notify_failed_processing',
+        dataset_config=dataset_config,
+        dag=dag
+    )
+    neuro_morphometric_atlas_pipeline.set_upstream(upstream_step.task)
+
+    neuro_morphometric_atlas_pipeline.doc_md = dedent("""\
+            # NeuroMorphometric Pipeline
+
+            SPM function: __%s__
+
+            This function computes an individual Atlas based on the NeuroMorphometrics Atlas. This is based on the
+            NeuroMorphometrics Toolbox.
+            This delivers three files:
+
+            1. Atlas File (*.nii);
+            2. Volumes of the Morphometric Atlas structures (*.txt);
+            3. CSV File (.csv) containing the volume, globals, and Multiparametric Maps (R2*, R1, MT, PD) for each
+            structure defined in the Subject Atlas.
+
+            The atlas is calculated locally and finally copied to a remote folder:
+
+            * Local folder: %s
+            * Remote folder: %s
+
+            Depends on: __%s__
+            """ % (spm_function, local_folder, server_folder, upstream_step.task_id))
+
+    return Step(upstream_step.task, upstream_step.task_id, upstream_step.priority_weight)
