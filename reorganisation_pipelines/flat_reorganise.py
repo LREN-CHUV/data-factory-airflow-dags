fdaafb19ebed951b2851dffc5a54e165153e7408
Mirco Nasuti <mirco.nasuti@chuv.ch>
2017-04-24 11:26:42 +0200

    remove unused code

--- reorganisation_pipelines/flat_reorganise.py
+++ reorganisation_pipelines/flat_reorganise.py
@@ -8,9 +8,6 @@ from re import fullmatch
 from airflow_scan_folder.operators.scan_folder_operator import ScanFlatFolderOperator
 
 
-ACCEPTED_FOLDERS = ['IMAGES']
-
-
 def flat_reorganisation_dag(dataset, folder, email_errors_to, trigger_dag_id, depth=1, folder_filter="*"):
 
     start = datetime.utcnow()
